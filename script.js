47affe302c39049003ac407ad3d43dd034f3132e
git12nazneen <lipinazneen65@gmail.com>
2024-03-01 23:45:05 +0600

    dynamic

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const btnContainer = document.getElementById('btn-container')
 const cardContainer = document.getElementById('card-container')
 
+let selectedCatagory = 1000;
+
 const fetchCategories = async() =>{
    const res = await fetch(`https://openapi.programming-hero.com/api/videos/categories`)
    const data = await res.json();
@@ -31,29 +33,34 @@ const fetchDataCatagories = async(catagoryID) =>{
 }
 
 const showSingleDataCard = (allData) =>{
-    console.log(allData)
+    // console.log(allData)
     cardContainer.innerHTML= ''
     allData.forEach(singleData =>{
-
+        console.log(singleData)
+        let verifiedBadge = ''
+        if(singleData.authors[0].verified){
+          verifiedBadge = `  <img class="w-6 h-6" src="./png-transparent-verified.png" alt="">`
+        }
         const newCard = document.createElement('div')
     newCard.innerHTML = `
     <div class="card w-full bg-base-100 shadow-xl">
     <figure class="overflow-hidden h-72">
-        <img class="w-full" src="./bike1.png" alt="Shoes" />
+        <img class="w-full" src="${singleData.thumbnail}" alt="Shoes" />
         <h6 class="absolute bottom-[40%] right-12">0 hr</h6>
     </figure>
     <div class="card-body">
         <div class="flex space-x-4 justify-start items-start">
             <div>
-                <img class="w-12 h-12 rounded-full" src="./bike1.png" alt="Shoes" />
+                <img class="w-12 h-12 rounded-full" src="${singleData.authors[0].profile_picture}" alt="Shoes" />
             </div>
             <div>
-                <h2 class="card-title">Laugh At My Pain</h2>
+                <h2 class="card-title">${singleData.title}</h2>
                 <div class="flex mt-3">
-                    <p class="">Author Name</p>
-                    <img class="w-6 h-6" src="./bike1.png" alt="">
+                    <p class="">${singleData.authors[0].profile_name}</p>
+                   ${verifiedBadge}
+                  
                 </div>
-                <p class="mt-3">100k Views</p>
+                <p class="mt-3">${singleData.others.views}</p>
             </div>
         </div>
     </div>
@@ -61,7 +68,10 @@ const showSingleDataCard = (allData) =>{
  `;
  cardContainer.appendChild(newCard)
     })
-  
+//   authors
+
 }
 
+
 fetchCategories()
+fetchDataCatagories(selectedCatagory)
